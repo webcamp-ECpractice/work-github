62fc36a3e98a09e6baf3d5944bd22cf2cb8ab87d
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-04-30 20:29:58 +0000

    ADD管理者商品登録、一覧、詳細、編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
+
+  has_one_attached :image
+
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+
+  def add_tax_price
+    (self.price * 1.08).round
+  end
+
 end
