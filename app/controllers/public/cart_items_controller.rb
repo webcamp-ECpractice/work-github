9ca6d4135a21e14ab4cb96edb30367ae1f4f450e
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-05-02 10:23:41 +0000

    UPDATE商品詳細からカート追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,28 @@
 class Public::CartItemsController < ApplicationController
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
+  def create
+   cart_item = CartItem.new(cart_item_params)
+   cart_item.save
+   redirect_to cart_items_path
+  end
+
+ private
+
+ def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+ end
+
 end
+
