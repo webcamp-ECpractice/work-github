3d989c0aa7524d513f611b78cd5ff5851cf8d2ef
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-05-02 10:54:18 +0000

    ADD会員情報一覧、編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,9 +5,23 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :email ,:is_deleted)
   end
 
 end
