e7972d603fb359848954ffe999ad1043b47042a8
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-05-05 12:00:09 +0000

    Add注文履歴一覧

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,19 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+
+  enum payment_method: {
+    credit:1, #クレジットカード
+    bank:2 #銀行振込
+  }
+
+  enum is_ordered: {
+    no_payment:1, #入金待ち
+    ok_payment:2, #入金確認
+    making:3, #動作中
+    preparing_delivery:4,#発送準備中
+    delivered:5 #発送済み
+  }
+
+
 end
