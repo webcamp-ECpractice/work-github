d0cc000825c2528959b14eabdd6f9f976750de5f
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-05-05 06:18:06 +0000

    ADDカート追加、表示、編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,18 +1,31 @@
 class Public::CartItemsController < ApplicationController
 
   def index
+    @cart_item = CartItem.where(customer_id: current_customer.id)
+    @total = @cart_item.inject(0) {|sum,item| sum + item.subtotal }
   end
 
   def update
+    @cart_item = CartItem.where(customer_id: current_customer.id)
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
+    cart_item = CartItem.where(customer_id: current_customer.id)
+    cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
+    cart_item = CartItem.where(customer_id: current_customer.id)
+    cart_item.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
+   if  
+   
    cart_item = CartItem.new(cart_item_params)
    cart_item.save
    redirect_to cart_items_path
