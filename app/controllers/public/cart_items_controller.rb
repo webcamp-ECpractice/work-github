19345d088ae4977d7fbb5208d188d314b0660dea
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-05-06 13:13:39 +0000

    update　カート内update機能編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,7 +6,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
-    @cart_item = CartItem.where(customer_id: current_customer.id)
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
