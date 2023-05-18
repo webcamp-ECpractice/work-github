4b39fca3e8c4369900130a9e69701a60c7cfe080
yssmmr <125275634+yssmmr@users.noreply.github.com>
2023-05-18 12:26:41 +0000

    バリデーション、管理者側レイアウト作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_customer!, except: [:top,:index,:about]
-  # before_action :authenticate_admin!, except: [:top,:index,:about]
 
 end
